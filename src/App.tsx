bdb88280f927bb05759e70f9a3b5e6a05d7d525b
Shubhojeet Bera <shubhojeet.bera@iterate-ai.com>
2024-12-17 15:01:50 +0530

    test - 1

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
         <p>Hello by Shubho</p>
       </div>
       <p className="read-the-docs">
-        Made by Shubhojxfdfsfdfeaderfe as dfdaadfadfet fdfaad B asdadafsera xfadf - sdaadf2333dffadfaadffda sxfasdf
+        Made by Shubhojxfdfsfdfeaderfe dddasdsf as dfdaadfadfet fdfaad B asdadafsera xfadf - sdaadf2333dffadfaadffda sxfasdf
       </p>
     </>
   )
