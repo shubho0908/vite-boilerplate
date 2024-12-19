7dc00196694bbc9faf8249a146e2c0dbf38b8a58
Shubhojeet Bera <shubhojeet.bera@iterate-ai.com>
2024-12-19 15:26:37 +0530

    test - 1

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-        <p>Hello by Shubhojeet Bera - From Durg</p>
+        <p>Hello by Shubhojeet Bera - From Durg, CG</p>
       </div>
       
     </>
